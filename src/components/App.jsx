ba8082d0a1a4a0de9b26eb43f0e504fe89025918
Birendra Hansda <virhansda2302@gmail.com>
2025-07-25 23:14:17 +0530

    created components

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Form from "./Form"
+import Output from "./Output"
+
+export default function App(){
+    return (
+        <>
+        <Form />
+        <Output />
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import '../src/style.css'
+import App from "./components/App"
 
 createRoot(document.getElementById('root')).render(
     <App />
