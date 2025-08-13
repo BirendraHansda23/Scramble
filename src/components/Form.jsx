988c25cba0bedb51a86db9a6a90cc35f852e7e34
Birendra Hansda <virhansda2302@gmail.com>
2025-08-13 23:26:28 +0530

    Updated output area

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,37 @@
 import { useState } from "react"
+import { SHA256 } from "crypto-js";
+import { SHA3 } from "crypto-js";
+import { AES } from "crypto-js";
+import { DES } from "crypto-js";
 
 export default function Form(){
 
     const [plainText,setPlainText] = useState("");
-    const [cipherText,setCipherText] = useState("");
+    const [cipher, setCipher] = useState({ text: "", key: null });
+    const [method, setMethod] = useState({ methodName:"", reversible: null });
 
     function handleSubmit(e){
         e.preventDefault();
         console.log("Plain text:", plainText);
+        console.log(method);
+        
+        if (method.methodName === "sha256") {
+            setCipher({ text: SHA256(plainText).toString(), key: method.reversible?"key": null});
+        }
+
+        if (method.methodName === "sha3") {
+            setCipher({ text: SHA3(plainText).toString(), key: method.reversible?"key": null});
+        }
+
+        if (method.methodName === "aes") {
+        // AES encryption placeholder
+        }
+
+        if (method.methodName === "des") {
+        // DES encryption placeholder
+        }
+        
+        console.log(cipher);
     }
 
     return (
@@ -15,20 +39,52 @@ export default function Form(){
         <section className="flex justify-center">
             <article className="flex flex-col max-w-[800px] p-4">
                 <form onSubmit={handleSubmit} className="flex flex-col justify-center">
-                <input type="text" 
-                className="border-1 rounded-md min-w-[400px] min-h-[200px] p-2 my-2"
-                placeholder="Enter your message here..." 
-                value={plainText} 
-                onChange={(e) => {setPlainText(e.target.value)}}
-                aria-label="input text" required />
-                <button type="submit" 
-                className=" rounded-md bg-gray-700 text-white p-3"
-                 >Encrypt</button>
+
+                    <label htmlFor="method">Choose an encryption method </label>
+                    <select
+                    name="method"
+                    id="method"
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        const reversibleMethods = ["aes","des"];
+                        const rev = reversibleMethods.includes(value)? true : false;
+                        setMethod({ methodName: value, reversible: rev });
+                    }}
+                    autoFocus
+                    required
+                    >
+                    <optgroup label="Reversible">
+                        <option value="aes">AES</option>
+                        <option value="des">DES</option>
+                    </optgroup>
+                    <optgroup label="Irreversible">
+                        <option value="sha256">SHA256</option>
+                        <option value="sha3">SHA3</option>
+                    </optgroup>
+                    </select>
+
+                    <input type="text" 
+                    className="border-1 rounded-md min-w-[400px] min-h-[200px] p-2 my-2"
+                    placeholder="Enter your message here..." 
+                    value={plainText} 
+                    onChange={(e) => {setPlainText(e.target.value)}}
+                    aria-label="input text" required />
+
+
+                    <button type="submit" 
+                    className=" rounded-md bg-gray-700 text-white p-3"
+                    >Encrypt</button>
                 </form>
 
-                <div className="border-1 rounded-md min-w-[400px] min-h-[200px] p-2 my-2">
-                    <p>{cipherText}</p>
-                </div>
+                {cipher.text && <><div className="border-1 rounded-md min-w-[400px] min-h-[200px] p-2 my-2">
+                     <p>{cipher.key && cipher.key}{cipher.text}</p> 
+                    </div>
+                    <button type="submit" 
+                        className=" rounded-md bg-gray-900 text-white py-3 px-2"
+                    >Copy to clip</button>
+                    </>
+                }
+                
             </article>
 
         </section>
--- src/utils/cryptoUtils.js
+++ src/utils/cryptoUtils.js
@@ -1,10 +1,8 @@
-import sha256 from "crypto-js/sha256";
-import hmacSHA512 from "crypto-js/hmac-sha512";
-import Base64 from "crypto-js/enc-base64";
+import CryptoJS from "crypto-js";
 
 export function caesarEncrypt(text, shift) {
   return text.replace(/[a-z]/gi, (char) => {
-    const base = (char=== char.toUpperCase()) ? 65 : 97;
+    const base = char === char.toUpperCase() ? 65 : 97;
     return String.fromCharCode(
       ((char.charCodeAt(0) - base + shift) % 26) + base
     );
@@ -13,4 +11,14 @@ export function caesarEncrypt(text, shift) {
 
 export function caesarDecrypt(text, shift) {
   return caesarEncrypt(text, 26 - (shift % 26)); // Reverse shift
+}
+
+export function SHA256(text) {
+  var hash = CryptoJS.SHA256(text);
+  return hash;
+}
+
+export function SHA512(text) {
+  var hash = CryptoJS.SHA3(text);
+  return hash;
 }
